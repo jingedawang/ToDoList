62eaad61cf6a8fefe5ad3649553e47d0edde365a
jinge <wjg172184@163.com>
2017-07-01 17:12:37 +0800

    创建工程。 设计初始界面，只放了一个ListView。 数据保存在data.xml中。 目前只做到了数据源到UI的单向绑定，使用LINQ的检索结果作为Binding源。 布局、双向数据绑定都是大大的难题啊。。。需要继续学习。

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using ToDoList.Model;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            XDocument xdoc = XDocument.Load(System.Environment.CurrentDirectory + @"\Data\data.xml");
+            this.listViewToDoList.ItemsSource =
+                from element in xdoc.Descendants("Item")
+                select new ToDoItem()
+                {
+                    Content = element.Attribute("Content").Value,
+                };
+            
+        }
+    }
+}
